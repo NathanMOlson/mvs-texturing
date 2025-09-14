d257417ec09b952086a95d580744f87737198b74
Nathan <nathanmolson@gmail.com>
2025-09-14 10:24:36 -0700

    remove mvs-texturing header

--- apps/texrecon2/texrecon2.cpp
+++ apps/texrecon2/texrecon2.cpp
@@ -1,12 +1,3 @@
-/*
- * Copyright (C) 2015, Nils Moehrle
- * TU Darmstadt - Graphics, Capture and Massively Parallel Computing
- * All rights reserved.
- *
- * This software may be modified and distributed under the terms
- * of the BSD 3-Clause license. See the LICENSE.txt file for details.
- */
-
 #include <iostream>
 #include <fstream>
 #include <vector>
